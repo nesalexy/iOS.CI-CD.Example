589517e5382d4cec64b09c9cd4cc0ecb8b29eeb7
nesalexy <nesalexy@gmail.com>
2024-06-24 10:47:30 +0200

    init

---
+++ CiCdExample/CiCdExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdExampleApp.swift
+//  CiCdExample
+//
+//  Created by Alexy Nesterchuk on 24.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CiCdExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiCdExampleTests/CiCdExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CiCdExampleTests.swift
+//  CiCdExampleTests
+//
+//  Created by Alexy Nesterchuk on 24.06.2024.
+//
+
+import XCTest
+@testable import CiCdExample
+
+final class CiCdExampleTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(2+2, 4)
+    }
+
+}
